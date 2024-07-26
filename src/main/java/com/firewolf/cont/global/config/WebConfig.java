ee83f8fb1037f441354b2deca6a75c87d43b4611
JINHYUNTAEK <jht2512@naver.com>
2024-07-27 00:13:28 +0900

    cors setting

--- src/main/java/com/firewolf/cont/global/config/WebConfig.java
+++ src/main/java/com/firewolf/cont/global/config/WebConfig.java
@@ -12,11 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 
